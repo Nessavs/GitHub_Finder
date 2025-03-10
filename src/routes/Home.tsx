32317895d568b307d72f51935015855679533613
VanessaDaSilva <vanessa.silva@amo.delivery>
2025-03-10 11:57:24 -0300

    feat: add search user

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,22 @@
-import React from "react"
+import Search from "../componentes/Search"
+import { useState } from "react"
+import { UserProps } from "../types/user"
 
 const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null)
+
+    const loadUser = async (userName: string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json()
+
+        console.log(data)
+
+    }
+
     return (
         <div>
-            <h1>Welcome to the Home Page!</h1>
-            <p>This is the default page for the application.</p>
+            <Search loadUser={loadUser} />
         </div>
     )
 }

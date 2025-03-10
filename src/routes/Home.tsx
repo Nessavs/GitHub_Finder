d8f7e99a67cdacaba9d0e97164943857c436fff9
VanessaDaSilva <vanessa.silva@amo.delivery>
2025-03-10 15:56:29 -0300

    feat: error control

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,19 +1,27 @@
 import Search from "../components/Search"
 import { useState } from "react"
 import { UserProps } from "../types/user"
-import  User from "../components/User"
+import User from "../components/User"
+import Error from "../components/Error"
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`)
 
     const data = await res.json()
 
-    console.log(data)
+    if (res.status === 404) {
+      setError(true)
+      return
+    }
 
-    const {avatar_url, login, location, followers, following} = data
+    const { avatar_url, login, location, followers, following } = data
     const userData: UserProps = {
       avatar_url,
       login,
@@ -28,7 +36,8 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <User {...user}/>}
+      {user && <User {...user} />}
+      {error && <Error />}
     </div>
   )
 }
